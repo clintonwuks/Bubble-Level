95bd0ba0477c5750538b5074c51d7b1a7c31dc91
Ikenna Clinton Onwuka <ikennaclinton.onwuka@student.griffith.ie>
2021-01-18 01:05:51 +0000

    Bracket 2 done

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,44 +18,68 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager sm;
     private float rotation_matrix[] = new float[16];
     private float orientation_values[] = new float[4];
+    private float bearings[] =new float[500];
+    private float pitch[] = new float[500];
+    private float roll[] = new float[500];
+    private int count= 500;
+    private int count2= 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // get access to the text view and the sensor manager
-//        tv = (TextView) findViewById(R.id.tv);
-//        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-//// attach a sensor to the rotation vector
-//
-//        Configuration c = getResources().getConfiguration();
+        tv = (TextView) findViewById(R.id.tv);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+// attach a sensor to the rotation vector
+
+        Configuration c = getResources().getConfiguration();
 
 
 
-//        sm.registerListener(new SensorEventListener() {
-//                                public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//                                }
-//                                public void onSensorChanged(SensorEvent event) {
-//// get the new rotation matrix and convert them into orientation
-////values
-//                                    SensorManager.getRotationMatrixFromVector(rotation_matrix,
-//                                            event.values);
-//                                    SensorManager.getOrientation(rotation_matrix, orientation_values);
-//// as the orientation values are unitless we need to convert themback
-//// into degrees
-//                                    orientation_values[0] = (float)
-//                                            Math.toDegrees(orientation_values[0]);
-//                                    orientation_values[1] = (float)
-//                                            Math.toDegrees(orientation_values[1]);
-//                                    orientation_values[2] = (float)
-//                                            Math.toDegrees(orientation_values[2]);
-//// print out the values to the text view
-//                                    tv.setText(
-//                                            orientation_values[0]
-//                                                    + " , " + orientation_values[1] +" , "
-//                                                    + " , " + orientation_values[2]);
-//                                }
-//                            }, sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
-//                SensorManager.SENSOR_DELAY_UI);
+        sm.registerListener(new SensorEventListener() {
+                                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                                }
+                                public void onSensorChanged(SensorEvent event) {
+// get the new rotation matrix and convert them into orientation
+//values
+                                    SensorManager.getRotationMatrixFromVector(rotation_matrix,
+                                            event.values);
+                                    SensorManager.getOrientation(rotation_matrix, orientation_values);
+// as the orientation values are unitless we need to convert themback
+// into degrees
+
+                                   // for (int i = 0; i < count; i++) {
+                                        orientation_values[0] = (float)
+                                                Math.toDegrees(orientation_values[0]);
+                                        orientation_values[1] = (float)
+                                                Math.toDegrees(orientation_values[1]);
+                                        orientation_values[2] = (float)
+                                                Math.toDegrees(orientation_values[2]);
+// print out the values to the text view
+                                        bearings[count2]=orientation_values[0];
+                                        pitch[count2]=orientation_values[1];
+                                        roll[count2]=orientation_values[2];
+
+                                    Log.d("TAG", "onSensorChanged: for" + count2 +bearings[count2]+" , "+ pitch[count2]+" , "+roll[count2]);
+
+                                        if (count2 == 499) {
+                                            count2 = 0;
+                                        } else {
+                                            count2++;
+                                        }
+
+                                   // }
+                                    tv.setText(
+                                            orientation_values[0]
+                                                    + " , " + orientation_values[1] +" , "
+                                                    + " , " + orientation_values[2]);
+                                }
+
+
+
+
+                            }, sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+                SensorManager.SENSOR_DELAY_UI);
     }
 }
